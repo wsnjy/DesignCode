3e37a10da3e8ee4460a887855d2986547874d8b7
wisnu <overdongo@gmail.com>
2020-01-29 12:02:09 +0700

    add ring animation

--- DesignCode/ContentView.swift
+++ DesignCode/ContentView.swift
@@ -82,7 +82,7 @@ struct ContentView: View {
                 }
             )
             
-            BottomCardView()
+            BottomCardView(show: $showCard)
                 .offset(x: 0, y: showCard ?  360 : 1000)
                 .offset(y: bottomState.height)
                 .blur(radius: show ? 20 : 0)
@@ -172,16 +172,38 @@ struct TitleView: View {
 }
 
 struct BottomCardView: View {
+    
+    @Binding var show: Bool
+    
     var body: some View {
         VStack(spacing:20){
+            
             Rectangle()
                 .frame(width:40, height: 5)
                 .cornerRadius(3)
                 .opacity(0.1)
+            
             Text("This certificate is proof that Wisnu has achieved the UI Design course with approval from a Design+Code instructor." )
                 .multilineTextAlignment(.center)
                 .font(.subheadline)
                 .lineSpacing(4)
+            
+            HStack(spacing: 20.0) {
+                RingView(color1: #colorLiteral(red: 0.2392156869, green: 0.6745098233, blue: 0.9686274529, alpha: 1), color2: #colorLiteral(red: 0.1764705926, green: 0.01176470611, blue: 0.5607843399, alpha: 1), width: 88, height: 88, percent: 78, show: $show)
+                    .animation(Animation.easeInOut.delay(0.3))
+                VStack(alignment: .leading, spacing: 8.0) {
+                    Text("SwiftUI").fontWeight(.bold)
+                    Text("12 of 12 sections completed\n10 hours spent so far")
+                        .font(.footnote)
+                        .foregroundColor(.gray)
+                        .lineSpacing(4)
+                }
+                .padding(20)
+                .background(Color.white)
+                .cornerRadius(20)
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: 0, y: 10)
+            }
+            
             Spacer()
         }
         .padding(.top, 8)
